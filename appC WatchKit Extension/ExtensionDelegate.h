198e706adeb7096c3a1403b9bf32964e2d2c97f4
john <john@goodstadt.me.uk>
2015-08-31 21:09:44 +0100

    Initial Commit

---
+++ appC WatchKit Extension/ExtensionDelegate.h
@@ -0,0 +1,13 @@
+//
+//  ExtensionDelegate.h
+//  appC WatchKit Extension
+//
+//  Created by john on 31/08/2015.
+//  Copyright © 2015 john goodstadt. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+@interface ExtensionDelegate : NSObject <WKExtensionDelegate>
+
+@end
---
+++ appC WatchKit Extension/InterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  InterfaceController.h
+//  appC WatchKit Extension
+//
+//  Created by john on 31/08/2015.
+//  Copyright © 2015 john goodstadt. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface InterfaceController : WKInterfaceController
+
+@end
